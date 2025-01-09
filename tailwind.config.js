fd24396b071450eccfd205025dd0c53782d0c5bd
saurabh dubey <dubeysaurabh310@gmail.com>
2025-01-09 08:53:17 +0530

    converted website to tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'], // Update paths to match your project structure
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
